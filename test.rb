627b2b42690241c4fa9aca4de9fa0e771cf2cce1
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-19 16:53:04 +0000

    Automatically backed up by Learn

---
+++ test.rb
@@ -0,0 +1,14 @@
+def assign_rooms(speaker_list)
+  room_assignments = []
+  room_range = (1..7)
+  for i in speaker_list
+    for j in room_range
+    room_assign = "Hello, #{speaker_list[i]}! You'll be assigned to room #{j}!"
+    room_assignments << room_assign
+    end
+  end
+  return room_assigments
+end
+
+list = ["matt", "john"]
+assign_rooms()
