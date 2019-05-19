bb7f7899bf29b7c2909461cb7ff70580780c43be
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-19 17:29:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -30,10 +30,8 @@ end
 def printer(names)
   for i in names
     output_badge = batch_badge_creator(i)
-    puts output_badge
-  end
-  for j in names
     output_room = assign_rooms(i)
+    puts output_badge
     puts output_room
   end
 end
