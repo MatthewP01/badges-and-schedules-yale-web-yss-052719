5f740a32a06ba08fac40968958bed92d6ec855fd
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-19 16:49:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,6 +20,6 @@ def assign_rooms(speaker_list)
   room_range = (1..7)
   for i in speaker_list
     for j in room_range
-    "Hello, _____! You'll be assigned to room _____!"
+    "Hello, #{speaker_list[i]}! You'll be assigned to room _____!"
 
 end
