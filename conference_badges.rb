d23ef9b7887128e549537cd80708ee799519a579
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-19 16:25:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 def batch_badge_creator(array_names)
-  array_names = ["Edsger, Ada, Charles, Alan, Grace, Linus, and Matz"]
+  array_names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", and "Matz"]
 end
 
 def assign_rooms
