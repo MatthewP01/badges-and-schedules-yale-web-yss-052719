a70167c82a4a0d2a4139888817386f6a553b8b18
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-19 17:19:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,7 +23,6 @@ def assign_rooms(speaker_list)
     room_assign = "Hello, #{i}! You'll be assigned to room #{room_range}!"
     room_range += 1
     room_assignments << room_assign
-    # end
   end
   return room_assignments
 end
