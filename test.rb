6945a9aeb727a64bf0e782d2dffbbb7e50b4150e
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-19 17:00:55 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -3,7 +3,6 @@ def assign_rooms(speaker_list)
   # room_range = ('1'..'7')
   room_no = 0
   for i in speaker_list
-
     "Hello, #{speaker_list[i]}! You'll be assigned to room #{j}!"
     for j in room_range
     room_assign = "Hello, #{speaker_list[i]}! You'll be assigned to room #{j}!"
